587f74db23dbf87f3701f4b7c8f2be088a407955
Archetapp <archetapp@gmail.com>
2016-10-11 08:09:06 -0600

    Initial Commit

---
+++ Pong2Tests/Pong2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Pong2Tests.swift
+//  Pong2Tests
+//
+//  Created by Jared Davidson on 10/11/16.
+//  Copyright © 2016 Archetapp. All rights reserved.
+//
+
+import XCTest
+@testable import Pong2
+
+class Pong2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
