467b8dc8529f82bdac70353f33f971ce06bedce0
Archetapp <archetapp@gmail.com>
2016-11-02 17:20:32 -0600

    Part 3

--- Pong2/GameScene.swift
+++ Pong2/GameScene.swift
@@ -15,7 +15,18 @@ class GameScene: SKScene {
     var enemy = SKSpriteNode()
     var main = SKSpriteNode()
     
+    var topLbl = SKLabelNode()
+    var btmLbl = SKLabelNode()
+    
+    var score = [Int]()
+    
     override func didMove(to view: SKView) {
+        
+        startGame()
+        
+        topLbl = self.childNode(withName: "topLabel") as! SKLabelNode
+        btmLbl = self.childNode(withName: "btmLabel") as! SKLabelNode
+        
         
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
@@ -29,14 +40,36 @@ class GameScene: SKScene {
         border.restitution = 1
         
         self.physicsBody = border
+    }
+    
+    func startGame() {
+        score = [0,0]
+        topLbl.text = "\(score[1])"
+        btmLbl.text = "\(score[0])"
+    }
 
+    func addScore(playerWhoWon : SKSpriteNode){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoWon == main {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        }
+        else if playerWhoWon == enemy {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
+        
+        topLbl.text = "\(score[1])"
+        btmLbl.text = "\(score[0])"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch in touches {
             let location = touch.location(in: self)
-            
             
             main.run(SKAction.moveTo(x: location.x, duration: 0.2))
             
@@ -58,6 +91,12 @@ class GameScene: SKScene {
         
         enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
         
+        if ball.position.y <= main.position.y - 70 {
+            addScore(playerWhoWon: enemy)
+        }
+        else if ball.position.y >= enemy.position.y + 70 {
+            addScore(playerWhoWon: main)
+        }
     }
 }
 
